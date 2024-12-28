4acd747c673bffca93b85c0e05ef6a8df53303c2
Angeliki Fokou <a@fokou.gr>
2024-12-28 18:59:36 +0200

    Expand/hide blog details

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,31 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author.name}
-  </div>  
-)
+import { useState } from 'react';
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const [expanded, setExpanded] = useState(false)
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} <button onClick={() => setExpanded(!expanded)}>{expanded ? 'hide' : 'view'}</button>
+      </div>
+      {expanded && (
+        <div>
+          <div>{blog.url}</div>
+          <div>
+            likes {blog.likes} <button>like</button>
+          </div>
+          <div>{blog.author.name}</div>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
